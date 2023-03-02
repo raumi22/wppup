39e9ab417bd0563efc5d0fab3dcc28677a251d30
raumi22 <82218429+raumi22@users.noreply.github.com>
2023-03-02 19:47:38 +0100

    Update salts.sh

--- salts.sh
+++ salts.sh
@@ -1,35 +1,26 @@
 #!/bin/bash
 
-# Set the file path
-FILE_PATH="/var/www/html/wordpress/wp-config.php"
-
-# Check if the file exists
-if [ -f "$FILE_PATH" ]; then
-  # Set the unique key phrase
-  KEY_PHRASE="put your unique phrase here"
-
-  # Find all instances of the key phrase in the file
-  INSTANCES=$(grep -o "$KEY_PHRASE" "$FILE_PATH")
-
-  # Loop through each instance of the key phrase
-  while read -r INSTANCE; do
-    # Generate a new 64-character string for each instance
-    NEW_STRING=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
-
-    # Replace the instance of the key phrase with the new string
-    sed -i "s/$INSTANCE/$NEW_STRING/g" "$FILE_PATH"
-
-    # Print the new string for the instance
-    echo "New string for instance '$INSTANCE': $NEW_STRING"
-  done <<< "$INSTANCES"
-else
-  echo "File not found at $FILE_PATH."
-fi
-
+# Set unique key phrases
+AUTH_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+SECURE_AUTH_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+LOGGED_IN_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+NONCE_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+AUTH_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+SECURE_AUTH_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+LOGGED_IN_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
+NONCE_SALT=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64 ; echo '')
 
 # Find and replace key phrases in wp-config.php file
 if [ -f /var/www/html/wordpress/wp-config.php ]; then
-  sed -i "s/<?php/<?php\ndefine('WP_FAIL2BAN_PROXIES','10.1.0.100');\nif (\$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') \$_SERVER['HTTPS']='on';/" "$FILE_PATH"
+  sed -i "s/define( 'AUTH_KEY',         'put your unique phrase here' );/define( 'AUTH_KEY', '$AUTH_KEY' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );/define( 'SECURE_AUTH_KEY', '$SECURE_AUTH_KEY' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'LOGGED_IN_KEY',    'put your unique phrase here' );/define( 'LOGGED_IN_KEY', '$LOGGED_IN_KEY' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'NONCE_KEY',        'put your unique phrase here' );/define( 'NONCE_KEY', '$NONCE_KEY' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'AUTH_SALT',        'put your unique phrase here' );/define( 'AUTH_SALT', '$AUTH_SALT' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );/define( 'SECURE_AUTH_SALT', '$SECURE_AUTH_SALT' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'LOGGED_IN_SALT',   'put your unique phrase here' );/define( 'LOGGED_IN_SALT',   '$LOGGED_IN_SALT' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/define( 'NONCE_SALT',       'put your unique phrase here' );/define( 'NONCE_SALT',       '$NONCE_SALT' );/" /var/www/html/wordpress/wp-config.php
+  sed -i "s/<?php/<?php\ndefine('WP_FAIL2BAN_PROXIES','10.1.0.100');\nif (\$_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') \$_SERVER['HTTPS']='on';/" /var/www/html/wordpress/wp-config.php
 else
   echo "wp-config.php file not found in /var/www/html/wordpress directory."
 fi
